4ad333c75779635662afa610e8c8c222bdf4f10f
kabir <kabirsatpathy@gmail.com>
2014-02-02 13:15:55 +0530

    first commit for chking.

---
+++ MS/src/git/First/Test.java
@@ -0,0 +1,14 @@
+package git.First;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my git setup working fine now");
+
+	}
+
+}
